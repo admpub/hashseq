7720cec69974617304e7f4ca1c2a99c22aa50b27
Hank Shen <swhbox@foxmail.com>
2019-11-04 23:05:28 +0800

    update

--- id.go
+++ id.go
@@ -1,4 +1,4 @@
-// This package provides ID masking using hash ids.
+// Package hashseq This package provides ID masking using hash ids.
 //
 // Hash IDs are a string represnetation of numerical incrementing IDs,
 // obfuscating the integer value. For more information see
@@ -10,99 +10,120 @@ import (
 	"encoding/json"
 	"fmt"
 
-	hashid "github.com/speps/go-hashids"
+	hashid "github.com/admpub/go-hashids"
 )
 
-var HashData = &hashid.HashIDData{
-	Alphabet:  hashid.DefaultAlphabet,
-	MinLength: 4,
-	Salt:      "",
+var (
+	hashData = &hashid.HashIDData{
+		Alphabet:  hashid.DefaultAlphabet,
+		MinLength: 4,
+		Salt:      "",
+	}
+	hashID *hashid.HashID
+)
+
+// HashID .
+func HashID() *hashid.HashID {
+	if hashID == nil {
+		var err error
+		hashID, err = hashid.NewWithData(hashData)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return hashID
 }
 
-// Set the salt to use for ID obfuscation
+// SetSalt Set the salt to use for ID obfuscation
 func SetSalt(salt string) {
-	HashData.Salt = salt
+	if hashData.Salt == salt {
+		return
+	}
+	hashData.Salt = salt
+	var err error
+	hashID, err = hashid.NewWithData(hashData)
+	if err != nil {
+		panic(err)
+	}
 }
 
-type Id struct {
-	Int64 int64
-}
-
-func (id *Id) Int() int {
-	return int(id.Int64)
+// ID uint64
+type ID struct {
+	Uint64 uint64
 }
 
 // Return the hashid as an obfuscated string
-func (id *Id) String() string {
-	str, err := hashid.NewWithData(HashData).Encode([]int{id.Int()})
+func (id *ID) String() string {
+	str, err := HashID().EncodeUint64([]uint64{id.Uint64})
 	if err != nil {
 		return ""
 	}
 	return str
 }
 
-// Returns the hashid as a string fulfilling the json.Marshaller interface
-func (id Id) MarshalJSON() ([]byte, error) {
-	str, err := hashid.NewWithData(HashData).Encode([]int{id.Int()})
+// MarshalJSON Returns the hashid as a string fulfilling the json.Marshaller interface
+func (id ID) MarshalJSON() ([]byte, error) {
+	str, err := HashID().EncodeUint64([]uint64{id.Uint64})
 	if err != nil {
 		return nil, err
 	}
 	return json.Marshal(str)
 }
 
-// Unmarshal a string and decode into an integer
-func (id *Id) UnmarshalJSON(data []byte) error {
+// UnmarshalJSON Unmarshal a string and decode into an integer
+func (id *ID) UnmarshalJSON(data []byte) error {
 	decoded, err := Decode(data)
 	if err != nil {
 		return err
 	}
-	id.Int64 = int64(decoded)
+	id.Uint64 = uint64(decoded)
 	return nil
 }
 
 // Decode a hashid byte into an Id, setting its integer
-func Decode(hashid []byte) (id int64, err error) {
+func Decode(hashid []byte) (id uint64, err error) {
 	return DecodeString(string(hashid))
 }
 
-func MustDecodeString(hashid string) int64 {
+// MustDecodeString .
+func MustDecodeString(hashid string) uint64 {
 	i, err := DecodeString(hashid)
 	if err != nil {
 		panic(err.Error())
 	}
-	return int64(i)
+	return uint64(i)
 }
 
-// Decode a hashid string into an Id, setting its integer
-func DecodeString(h string) (id int64, err error) {
-	ints := hashid.NewWithData(HashData).Decode(h)
-	if len(ints) != 1 {
-		err = fmt.Errorf("Unexpected hashid value")
+// DecodeString Decode a hashid string into an Id, setting its integer
+func DecodeString(h string) (id uint64, err error) {
+	var ids []uint64
+	ids, err = HashID().DecodeUint64WithError(h)
+	if err != nil {
 		return
 	}
-	return int64(ints[0]), nil
+	return ids[0], nil
 }
 
-// Database scanning
-func (id *Id) Scan(value interface{}) (err error) {
-	var data int64
+// Scan Database scanning
+func (id *ID) Scan(value interface{}) (err error) {
+	var data uint64
 
 	// If the first four bytes of this are 0000
-	switch value.(type) {
+	switch v := value.(type) {
 	// Same as []byte
-	case int64:
-		data = value.(int64)
+	case uint64:
+		data = v
 	case nil:
 		return
 	default:
 		return fmt.Errorf("Invalid format: can't convert %T into id.Id", value)
 	}
 
-	id.Int64 = data
+	id.Uint64 = data
 	return nil
 }
 
-// This is called when saving the ID to a database
-func (id Id) Value() (driver.Value, error) {
-	return id.Int64, nil
+// Value This is called when saving the ID to a database
+func (id ID) Value() (driver.Value, error) {
+	return id.Uint64, nil
 }
